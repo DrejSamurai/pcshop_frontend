6b7e46da2c8086966e3ffd70b48f8c7dbcd68d27
Andrej Mishevski <andrej.mishevski@gmail.com>
2025-06-25 16:35:08 +0200

    image bugfix

--- src/pages/products/GpuPage.tsx
+++ src/pages/products/GpuPage.tsx
@@ -170,7 +170,7 @@ const GpuPage = () => {
                     >
                       <TableCell className="gpu-table-cell">
                         <img
-                          src={ProductService.getProxiedImageUrl(product.image)}
+                          src={product.image}
                           alt={product.title}
                           style={{ width: 100, height: 100, objectFit: 'cover' }}
                         />
